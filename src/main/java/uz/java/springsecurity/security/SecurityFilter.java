203c896ece82e535118da240dd07ccf35ad77e93
BekjoN <bezori0000@gmail.com>
2023-09-16 19:58:20 +0500

    update

---
+++ src/main/java/uz/java/springsecurity/security/SecurityFilter.java
@@ -0,0 +1,7 @@
+package uz.java.springsecurity.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class SecurityFilter extends OncePerRequestFilter {
+     @Override
+}
